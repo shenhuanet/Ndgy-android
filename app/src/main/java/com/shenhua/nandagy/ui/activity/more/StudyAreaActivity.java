7267dadbef96526cfee23cfe813fdcd90722028d
shenhuanet <shenhuanet@126.com>
2017-02-12 09:47:16 +0800

    modify.

--- app/src/main/java/com/shenhua/nandagy/ui/activity/more/StudyAreaActivity.java
+++ app/src/main/java/com/shenhua/nandagy/ui/activity/more/StudyAreaActivity.java
@@ -5,7 +5,6 @@ import android.support.annotation.Nullable;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
 
 import com.shenhua.nandagy.R;
@@ -38,29 +37,10 @@ public class StudyAreaActivity extends BaseActivity implements StudyListView {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-//            Window window = getWindow();
-//            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
-//                    | WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
-//            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-//                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-//                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
-//            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-//            window.setStatusBarColor(Color.TRANSPARENT);
-//            window.setNavigationBarColor(Color.TRANSPARENT);
-//        }
         setContentView(R.layout.activity_more_study);
         ButterKnife.bind(this);
-        setSupportActionBar(mToolbar);
-        ActionBar ab = getSupportActionBar();
-        assert ab != null;
-        ab.setDisplayHomeAsUpEnabled(true);
-        ab.setDisplayShowHomeEnabled(true);
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-//            mToolbar.setPadding(0, 40, 0, 0);
-//        }
+        setupActionBar("学习专区", true);
         initViewPager();
-
     }
 
     private void initViewPager() {
